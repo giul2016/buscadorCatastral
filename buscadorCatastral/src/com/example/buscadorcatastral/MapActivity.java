9a29299dc0d6d4882be7661ea6ea10ce709605a0
rafa <rafa@puigverd.org>
2013-12-04 00:46:25 +0100

    Con el mapview del mapa sin pasar las cordenadas

---
+++ buscadorCatastral/src/com/example/buscadorcatastral/MapActivity.java
@@ -0,0 +1,15 @@
+package com.example.buscadorcatastral;
+
+import com.example.buscadorcatastral2.R;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class MapActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_map);
+    }
+}
